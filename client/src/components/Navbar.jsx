3aeea6a30ab973550e63010354c0af76a5d7a2c4
Manjiri Milind Chavande <cmanjiri1912@gmail.com>
2022-10-08 18:54:21 +0530

    made the mobile navbar functional

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,61 +1,76 @@
 import React from "react";
-import {HiMenuAlt4} from 'react-icons/hi';
-import {AiOutlineClose} from 'react-icons/ai';
-import {Link} from "react-router-dom";
+import { HiMenuAlt4 } from 'react-icons/hi';
+import { AiOutlineClose } from 'react-icons/ai';
+import { Link } from "react-router-dom";
 
 import logo from '../../images/logo.png';
 
-const NavbarItem = ({title, classProps}) =>{
-    return(
+const NavbarItem = ({ title, classProps }) => {
+    return (
         <li className={`mx-4 cursor-pointer ${classProps}`}>
             {title}
         </li>
     );
 }
-const Navbar = () =>{
+const Navbar = () => {
 
     const [toggleMenu, setToggleMenu] = React.useState(false);
     return (
         <nav className='w-full flex md:justify-center justify-between items-center p-4'>
             <div className='md:flex-[0.5] flex-initial justify-center items-center'>
-                <img src={logo} alt = "logo" className='w-40 cursor-pointer'></img>
+                <img src={logo} alt="logo" className='w-40 cursor-pointer'></img>
             </div>
             <ul className='text-white md:flex hidden list-none flex-grow justify-end items-center flex-initial'>
-            {/* {[`${Market},${Exchange},${Tutorials},}`].map((item, index)=>( */}
-            <li className="  text-white px-3 py-2 rounded-md text-sm font-medium">
+                {/* {[`${Market},${Exchange},${Tutorials},}`].map((item, index)=>( */}
+                <li className="  text-white px-3 py-2 rounded-md text-sm font-medium">
                     <Link to="/">Home</Link>
-                 </li>
-                 <li className=" text-white px-3 py-2 rounded-md text-sm font-medium">
+                </li>
+                <li className=" text-white px-3 py-2 rounded-md text-sm font-medium">
                     <Link to="/market">Market</Link>
-                 </li>
-                 <li className=" text-white px-3 py-2 rounded-md text-sm font-medium">
+                </li>
+                <li className=" text-white px-3 py-2 rounded-md text-sm font-medium">
                     <Link to="/tutorial">Tutorial</Link>
-                 </li>
+                </li>
                 <li className='bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]'>
-                <a href="https://etherscan.io/">Wallet</a>
+                    <a href="https://etherscan.io/">Wallet</a>
                 </li>
             </ul>
             <div className='flex relative'>
                 {toggleMenu
-                ? <AiOutlineClose fontSize={28}className = "text-white md:hidden cursor-pointer" onClick={()=> setToggleMenu(false)}/>
-            :<HiMenuAlt4 fontSize={28}className = "text-white md:hidden cursor-pointer" onClick={()=> setToggleMenu(true)}/>} 
-            {toggleMenu && (
-                <ul 
-                className="z-10 fixed top-0 -right-2  p-3 w-[70vw] shadow-2xl md:hidden list-none
+                    ? <AiOutlineClose fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(false)} />
+                    : <HiMenuAlt4 fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(true)} />}
+                {toggleMenu && (
+                    <ul
+                        className="z-10 fixed top-0 -right-2  p-3 w-[70vw] shadow-2xl md:hidden list-none
                 flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white
                 animate-slide-in"
-                >
-                    <li className="text-xl w-full my-2">
-                        <AiOutlineClose onClick={()=> setToggleMenu(false)}></AiOutlineClose>
-                        {["Market","Exchange","Tutorials","Wallets"].map((item, index)=>(
+                    >
+                        <li className="text-xl w-full my-2">
+                            <AiOutlineClose onClick={() => setToggleMenu(false)}></AiOutlineClose>
+                            {/* {["Market","Exchange","Tutorials","Wallets"].map((item, index)=>(
                     <NavbarItem key={item + index} title = {item} classProps = "my-2 text-lg"/> 
-                ))}
-                    </li>
-                </ul>
-            )}
+                ))} */}
+                            <ul>
+                            <li className="text-white hover:text-white-200">
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li className="text-white hover:text-white-200">
+
+                                <Link to="/market">Market</Link>
+                            </li>
+                            <li className="text-white hover:text-white-200">
+                                <Link to="/tutorial">Tutorial</Link>
+                            </li>
+                            <li className="text-white hover:text-white-200">
+                                <a href="https://etherscan.io/">Wallet</a>
+                            </li>
+                            </ul>
+                        </li>
+                    </ul>
+                )}
             </div>
 
-            
+
         </nav>
     );
 }
