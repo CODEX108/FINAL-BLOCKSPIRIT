44e735775dee763f351d142dc18ab4ac6995bffd
anaskhan28 <khananasatique@gmail.com>
2022-10-23 18:10:13 +0530

    updated the syntax to react 18

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import{BrowserRouter} from "react-router-dom";
 import App from "./App";
 import { TransactionProvider  } from "./context/TransactionContext";
 import "./index.css";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <BrowserRouter>
   <TransactionProvider>
     <App />
   </TransactionProvider>
   </BrowserRouter>,
-  document.getElementById("root"),
 );
 
 
